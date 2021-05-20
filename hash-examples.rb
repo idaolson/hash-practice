dbfcc4dcd4a520f5c411c3c5bcbace97712798fa
Ida Olson <idaolson@gmail.com>
2021-05-19 21:37:24 -0600

    Add car hash

---
+++ hash-examples.rb
@@ -0,0 +1,11 @@
+car = {
+  "make" => "Volkswagen",
+  "model" => "Cabriolet",
+  "year" => 1986,
+  "color" => "red",
+  "has_auto_transmission" => false,
+  "number_of_owners" => 3,
+  "vin_number" => "WVWCA0155GK054321",
+  "repairs" => ['roof replace', 'engine rebuild', 'repaint']
+}
+puts car
